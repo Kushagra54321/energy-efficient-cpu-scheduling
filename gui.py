bb8ce1b456b7d86429fb19b7959ad59cecb866a5
Kushagra Singh <kushagrasingh54321@gmail.com>
2025-04-02 15:08:03 +0530

    printing the parameters related to the process

--- gui.py
+++ gui.py
@@ -8,3 +8,20 @@ class Process:
         self.burst_time = burst_time
         self.priority = priority
         self.power_consumption = power_consumption
+class SchedulerThread(QThread):
+    update_signal = pyqtSignal(str)
+
+    def _init_(self, processes):
+        super()._init_()
+        self.processes = processes
+
+    def run(self):
+        self.processes.sort(key=lambda x: (x.priority, x.power_consumption))
+        total_time = 0
+        
+        for process in self.processes:
+            self.update_signal.emit(f"Executing Process {process.pid}: Burst Time = {process.burst_time}, Power = {process.power_consumption}W")
+            time.sleep(1)
+            total_time += process.burst_time
+            
+        self.update_signal.emit(f"\nTotal Execution Time: {total_time} units")
