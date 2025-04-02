e4952c7d872d9c896aec3e38e89427107f10498b
Kushagra Singh <kushagrasingh54321@gmail.com>
2025-04-02 15:05:20 +0530

    initializing the process parameters

--- gui.py
+++ gui.py
@@ -2,3 +2,9 @@ import sys
 import time
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget, QTableWidgetItem, QLineEdit, QMessageBox)
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+class Process:
+    def _init_(self, pid, burst_time, priority, power_consumption):
+        self.pid = pid
+        self.burst_time = burst_time
+        self.priority = priority
+        self.power_consumption = power_consumption
