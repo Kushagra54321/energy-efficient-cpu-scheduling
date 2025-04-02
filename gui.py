8bbe91f87cfeb079b71f51c9bb4177e007088f23
Kushagra Singh <kushagrasingh54321@gmail.com>
2025-04-02 15:03:26 +0530

    importing the required libraries for gui

---
+++ gui.py
@@ -0,0 +1,4 @@
+import sys 
+import time
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QTableWidget, QTableWidgetItem, QLineEdit, QMessageBox)
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
